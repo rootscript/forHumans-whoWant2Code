dce3abe9f2dfd3ba96ae68912419cb963c47d24e
RootScript <rootscript@gmail.com>
2016-10-22 03:37:43 +0100

    added more verbose comments
    I hope i have got this right?

--- -elm-forHumans/elm-electron/src/Counter-v02.elm
+++ -elm-forHumans/elm-electron/src/Counter-v02.elm
@@ -8,12 +8,15 @@ import Html.App as App
 import Html.Events exposing (onClick)
 
 
+-- Our main function
+
+
 main =
     App.beginnerProgram { model = 0, view = view, update = update }
 
 
 
--- Model
+-- Model of our data structures, describing the type of data
 
 
 type alias Model =
@@ -21,16 +24,18 @@ type alias Model =
 
 
 
---model : Model
---model =
---  0
--- Update
+-- Messages that lists/name which functions will be used to create new values from our Model data
 
 
 type Msg
     = Increment
     | Decrement
     | Increment2
+
+
+
+-- Update Function ( takes in a Message, our data Model, & returns other data Models )
+-- it contains functions that are used in response to each Message case
 
 
 update msg model =
@@ -46,7 +51,9 @@ update msg model =
 
 
 
--- View
+-- View Function ( takes in a the new data Model, & returns html, using functions implemented in our Update Function to interact with our Viewed Model data )
+-- div & button are functions from our imported Html package
+-- onClick is a function imported from Html.Events, that allows the buttons to start functions from an onClick event
 
 
 view model =
@@ -54,5 +61,5 @@ view model =
         [ button [ onClick Decrement ] [ text "-" ]
         , div [] [ text (toString model) ]
         , button [ onClick Increment ] [ text "+" ]
-        , button [ onClick Increment2 ] [ text "++" ]
+        , button [ onClick Increment2 ] [ text "++++" ]
         ]
